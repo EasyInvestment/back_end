df776456803c16270f215db7f7b4f2e35f22cd4d
윤다은 <yunda-eun@yunda-eun-ui-MacBookPro.local>
2023-09-17 05:37:19 +0900

    Feat: 로그인 회원가입 완료

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'user' # 앱 네임스페이스 설정
+
 urlpatterns = [
-  path("login", views.login_view)
+  path("login", views.login_view, name="login"),
+  path("logout", views.logout_view, name="logout"),
+  path('signup/', views.signup_view, name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from .models import User
+from django.contrib import messages
 # Create your views here.
 
 
 def login_view(request):
   if request.method == "POST":
-    id = request.request.POST["id"]
-    pw = request.request.POST["pw"]
-    user = authenticate(id=id, pw=pw)
+    id = request.POST["id"]
+    pw = request.POST["pw"]
+    user = authenticate(username=id, password=pw)
     if user is not None:
       print("인증성공")
       login(request, user)
@@ -26,15 +27,18 @@ def logout_view(request):
 def signup_view(request):
 
   if request.method == "POST":
-    print(request.POST)
     name = request.POST["name"]
     phone = request.POST["phone"]
     pw = request.POST["pw"]
     pwc = request.POST["pwc"]
 
-    user = User.object.create_user(name, phone, pw)
-    user.pwc = pwc
-    user.save()
+    if pw == pwc:
+      user = User.objects.create_user(username=name, password=pw)
+      user.phone = phone  # phone 필드에 값을 할당
+      user.save()
+      login(request, user)  # 회원가입 후 자동으로 로그인
+      return redirect("user:login")
+    else:
+      messages.error(request, "비밀번호와 비밀번호 확인이 일치하지 않습니다.")
 
-    return redirect("user:login")
   return render(request, "users/signup.html")
