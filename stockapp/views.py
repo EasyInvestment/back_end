c7c2e657440310fe29fbe6b35870a4350c79b028
윤다은 <yunda-eun@yunda-eun-ui-MacBookPro.local>
2023-11-04 21:37:44 +0900

    Feat: 데이터 개수 1000개 제한

--- stockapp/views.py
+++ stockapp/views.py
@@ -6,5 +6,8 @@ from .monitor import monitor
 def stock_monitor(request):
     if request.method == 'GET':
         result = monitor()
-        print(result)
-        return Response({'data': result.to_dict(orient='records')})
+        # 데이터 프레임에서 처음 1000개의 로우만 선택
+        limited_result = result.head(1000)
+        print(limited_result)
+        # 선택된 로우들을 딕셔너리 리스트로 변환하여 JSON 응답으로 보냄
+        return Response({'data': limited_result.to_dict(orient='records')})
