f9d5db09240a71b0ce20b8791abdc7a442ff5d51
NAYYOUNGHEO <113881626+NAYYOUNGHEO@users.noreply.github.com>
2023-11-05 16:37:12 +0900

    그래프 연결

--- back_end-2/src/LineChart.js
+++ back_end-2/src/LineChart.js
@@ -7,57 +7,53 @@ const LineChart = () => {
     const [chartInitialized, setChartInitialized] = useState(false);
     const [error, setError] = useState(null);
 
+    // Fetch data from the server
     useEffect(() => {
         fetch('http://127.0.0.1:8000/stockapp/monitor/')
             .then(response => {
-                console.log('Server Response:', response);
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
                 }
                 return response.json();
             })
-            .catch(error => {
-                console.error('Fetch error: ', error);
-                setError(error.message);
-            })
             .then(dataObj => {
                 const actualData = dataObj.data || [];
                 console.log("Actual array data:", actualData);
 
+                // Assuming there is no 'datetime' field in the data received
                 const formattedData = actualData
-                    .filter(item => item && typeof item.datetime === 'string' && typeof item.close === 'number')
-                    .map(item => ({
-                        time: item.datetime.split(' ')[0],
+                    .filter(item => item && typeof item.close === 'number')
+                    .map((item, index) => ({
+                        time: index, // Replace this with your logic to assign time stamps
                         value: item.close
                     }));
-
 
                 setData(formattedData);
             })
             .catch(error => {
                 console.error('Fetch error: ', error);
+                setError(error.message);
             });
     }, []);
 
+    // Initialize the chart
     useEffect(() => {
         if (chartContainerRef.current && data.length > 0 && !chartInitialized) {
-            console.log('Initializing chart...');
             const chart = createChart(chartContainerRef.current, {
-                width: 400,
-                height: 300
+                width: chartContainerRef.current.clientWidth,
+                height: chartContainerRef.current.clientHeight
             });
             const lineSeries = chart.addLineSeries();
             lineSeries.setData(data);
             setChartInitialized(true);
-            console.log('Chart initialized and data set!');
         }
     }, [data, chartInitialized]);
 
     return (
-        <div ref={chartContainerRef} style={{ width: '400px', height: '300px' }}>
-        {data.length === 0 && !error && <p>Loading data...</p>}
-        {error && <p>Error: {error}</p>}
-    </div>
+        <div ref={chartContainerRef} style={{ width: '100%', height: '300px' }}>
+            {data.length === 0 && !error && <p>Loading data...</p>}
+            {error && <p>Error: {error}</p>}
+        </div>
     );
 };
 
